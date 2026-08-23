9e6cd8edb810e73dd15cb9af68f6d20e81e5b578
Vardani Gupta <thevardanigupta@gmail.com>
2026-08-23 06:54:23 +0530

    Complete Day 14 question 1

---
+++ Day-14/question27.c
@@ -0,0 +1,29 @@
+//Q27: Write a program to print the sum of the first n odd numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+9
+
+Input 2:
+5
+Output 2:
+25
+
+*/
+#include <stdio.h>
+int main()
+{
+    int n, num=1, sum = 0;
+    printf("Enter a number: \n");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        sum += num;
+        num+=2;
+    }
+    printf("Sum = %d\n", sum);
+    return 0;
+}
